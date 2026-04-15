fe3f6078f3c40686733dce25502b989d12f9aabc
Otavio72 <otavioribeiro1932004@gmail.com>
2026-04-15 15:01:46 -0300

    add: condicionando o codigo para apenas 1 envio de dados por volta

--- sockGRIDAUTO.py
+++ sockGRIDAUTO.py
@@ -8,7 +8,8 @@ sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 sock.bind((UDP_IP, UDP_PORT))
 
 print("🏎️ Aguardando Grid Autosport...")
-
+ultima_volta = 0
+ultimo_tempo_volta = 0
 while True:
     data, addr = sock.recvfrom(2048) # O pacote costuma ter ~64 bytes no modo 3
     
@@ -17,5 +18,14 @@ while True:
     telemetria = struct.unpack('f' * (len(data) // 4), data)
     velocidade_kmh = telemetria[7] * 3.6
     tempo_volta = telemetria[1]
-    volta_atual = int(telemetria[3])
-    print(f"Velocidade: {velocidade_kmh:.1f} km/h // tempo_volta: {tempo_volta} // volta_atual: {volta_atual}")
+    volta_numero = int(telemetria[3])
+
+    #print(f"tempo_volta: {tempo_volta}")
+    
+
+    if volta_numero != ultima_volta:
+        if volta_numero != -1:
+            print(f"tempo_volta: {ultimo_tempo_volta} // volta_numero: {ultima_volta}")
+        ultima_volta = volta_numero
+    ultimo_tempo_volta = tempo_volta
+        
