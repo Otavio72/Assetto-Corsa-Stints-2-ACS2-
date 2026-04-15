14a2e3679795a8e9ff8c9caeab88aa39ab338340
Otavio72 <otavioribeiro1932004@gmail.com>
2026-04-15 13:06:57 -0300

    add: bits necessarios mapeados para o sockF12013

--- sockF12013.py
+++ sockF12013.py
@@ -13,13 +13,21 @@ while True:
     # ACELERADOR teste3 = pacote[29]
     # FREIO teste5 = pacote[31]
     # POSICAO NA CORRIDA teste1 = pacote[39]
+    # quantidade de voltas da corrida teste1 = pacote[60]
+    # --- Processamento dos dados ---
+    vel_kmh    = pacote[7] * 3.6
+    tempo_lap  = pacote[1]
+    gas        = pacote[29] * 100  # Convertendo para 0-100%
+    brake      = pacote[31] * 100  # Convertendo para 0-100%
+    pos        = int(pacote[39])
+    total_laps = int(pacote[60])
 
-    teste1 = pacote[45]
-    teste2 = pacote[46]
-    teste3 = pacote[47]
-    teste4 = pacote[48]
-    teste5 = pacote[49]
-    teste6 = pacote[50]
+    # --- Organização Visual ---
+    # Criando barras visuais para os pedais (opcional, mas fica irado!)
+    bar_gas = "█" * int(gas / 10)
+    bar_brk = "█" * int(brake / 10)
 
-    #print()
-    print(f"teste: {teste1} // teste: {teste2}  // teste: {teste3} // teste: {teste4} // teste: {teste5} // teste: {teste6}")
+    # O print formatado
+    # \r no início faz ele voltar para o começo da linha
+    print(f" 🏁 POS: {pos:02d} | VOLTA: {tempo_lap:6.2f}s | VEL: {vel_kmh:5.1f} km/h | TOTAL: {total_laps:02d} ", end="")
+    print(f" | GAS: [{bar_gas:<10}] | FRE: [{bar_brk:<10}]", end="\r")
