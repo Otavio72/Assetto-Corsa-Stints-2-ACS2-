0bf9c2998f6f9c995685a251186af12384183dbb
Otavio72 <otavioribeiro1932004@gmail.com>
2026-05-28 15:14:08 -0300

    Adicionado mapeamento de carros e pistas na pasta mapeamentosf12013 organizados em arquivos .txt

--- sockF12013.py
+++ sockF12013.py
@@ -2,43 +2,43 @@ import socket
 import struct
 import time
 import json
-
+import os
 # DNA das pistas (Tamanho / Pico)
-MAPA_PISTAS = {
-    5301.926868: "AUSTRALIA"
-}
+#MAPA_PISTAS = {
+ #   5301.926868: "AUSTRALIA"
+#}
 
-MAPA_BOXES = {
-    "AUSTRALIA": {
-5129.36133: 'redbull1',
-5121.79102: 'redbull2',
-5144.31494: 'ferrari1',
-5136.74707: 'ferrari2',
-5157.30762: 'mclaren1',
-5149.73779: 'mclaren2',
-5170.30664: 'lotus1',
-5162.73682: 'lotus2',
-5185.24609: 'mercedez1',
-5177.67676: 'mercedez2',
-5198.24365: 'sauber1',
-5190.67529: 'sauber2',
-5211.25000: 'force1',
-5203.68164: 'force2',
-5226.18213: 'wilians1',
-5218.61182: 'wilians2',
-5239.17676: 'tororosso1',
-5231.60693: 'tororosso2',
-5252.18799: 'cartheran1',
-5244.61865: 'cartheran2',
-5267.1377: 'marrusia1',
-5259.56738: 'marrusia2',
-    }
-}
+#MAPA_BOXES = {
+ #   "AUSTRALIA": {
+#5129.36133: 'redbull1',
+#5121.79102: 'redbull2',
+#5144.31494: 'ferrari1',
+#5136.74707: 'ferrari2',
+#5157.30762: 'mclaren1',
+#5149.73779: 'mclaren2',
+#5170.30664: 'lotus1',
+#5162.73682: 'lotus2',
+#5185.24609: 'mercedez1',
+#5177.67676: 'mercedez2',
+#5198.24365: 'sauber1',
+#5190.67529: 'sauber2',
+#35211.25000: 'force1',
+#5203.68164: 'force2',
+#5226.18213: 'wilians1',
+#5218.61182: 'wilians2',
+#5239.17676: 'tororosso1',
+#5231.60693: 'tororosso2',
+#5252.18799: 'cartheran1',
+#5244.61865: 'cartheran2',
+#5267.1377: 'marrusia1',
+#5259.56738: 'marrusia2',
+ #   }
+#}
 
 # Variáveis de Estado
 valor_box_capturado = 0
 pico_atual = 0.0
-pista_confirmada = 0
+pista_confirmada = ""
 ultima_volta = 0
 ultima_volta_fechada = 0
 contador_voltas = 0
@@ -47,14 +47,89 @@ timer_pc = time.time()  # Relógio do PC guardando o início
 contador_pacotes_iguais = 0
 
 
-def buscar_carro(pista, valor_box):
-    # Procura o carro com uma margem de erro de 0.5 (meio metro)
-    boxes = MAPA_BOXES.get(pista, {})
-    for coord, nome in boxes.items():
-        if abs(coord - valor_box) < 0.5:
-            return nome
-    return "Carro Desconhecido"
+#def buscar_carro(pista, valor_box):
+ #   # Procura o carro com uma margem de erro de 0.5 (meio metro)
+  #  boxes = MAPA_BOXES.get(pista, {})
+   # for coord, nome in boxes.items():
+    #    if abs(coord - valor_box) < 0.5:
+     #       return nome
+    #return "Carro Desconhecido"
 
+
+# =========================================================================
+# 📂 DESCOBRIR PISTA
+# =========================================================================
+
+def descobrir_pista(pico_atual):
+
+    caminho = "MapeamentosF12013/Pistas/pistas.txt"
+
+    with open(caminho, "r", encoding="utf-8") as f:
+
+        for linha in f:
+
+            linha = linha.strip()
+
+            if not linha:
+                continue
+
+            nome_pista, dna = linha.split()
+
+            dna = float(dna)
+
+            # margem de erro
+            if abs(pico_atual - dna) < 2.0:
+
+                return nome_pista
+
+    return "PISTA_DESCONHECIDA"
+
+# =========================================================================
+# 🚗 DESCOBRIR CARRO
+# =========================================================================
+
+def buscar_carro(pista, valor_box):
+
+    caminho = f"MapeamentosF12013/Carros/boxes_{pista.lower()}.txt"
+
+    if not os.path.exists(caminho):
+        return "Carro Desconhecido"
+
+    with open(caminho, "r") as f:
+
+        for linha in f:
+
+            linha = linha.strip()
+
+            # ignora linha vazia
+            if not linha:
+                continue
+
+            try:
+
+                # separa no :
+                valor, nome = linha.split(":")
+
+                # limpa valor
+                #valor = float(valor.strip())
+                valor = round(float(valor.strip()), 5)
+
+                # limpa nome
+                nome = nome.strip()
+                nome = nome.replace("'", "")
+                nome = nome.replace(",", "")
+
+                # margem de erro
+                if abs(valor - valor_box) < 0.5:
+
+                    return nome
+
+            except Exception as e:
+
+                print(f"ERRO NA LINHA: {linha}")
+                print(e)
+
+    return "Carro Desconhecido"
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 sock.bind(("0.0.0.0", 20777))
@@ -74,71 +149,105 @@ def PegarCarroEpista():
 
     try:
         while True:
-            data, addr = sock.recvfrom(2048)
-            pacote = struct.unpack('f' * (len(data) // 4), data)
-            valor_atual = pacote[2]
 
-            # --- ETAPA A: CAPTURAR O BOX (SÓ RODA SE AINDA NÃO TEMOS O BOX) ---
+            data, addr = sock.recvfrom(2048)
+
+            pacote = struct.unpack(
+                'f' * (len(data) // 4),
+                data
+            )
+
+            #valor_atual = pacote[2]
+            valor_atual = round(pacote[2], 5)
+
+            # =========================================================
+            # 📦 CAPTURAR BOX
+            # =========================================================
+
             if valor_box_capturado == 0:
+
                 if valor_atual > 0 and valor_atual == v_antigo:
                     count_val += 1
                 else:
                     count_val = 0
-                
-                v_antigo = valor_atual # Atualiza para a próxima comparação
 
-                if count_val >= 15: # Aumentei pra 15 pra garantir
+                v_antigo = valor_atual
+
+                if count_val >= 15:
+
                     valor_box_capturado = valor_atual
-                    #print(f"\n✅ [BOX OK] Valor: {valor_box_capturado:.4f}")
+
                     print(
-                        f"\n✅ [BOX OK] Valor: {valor_box_capturado:.4f}",
+                        f"\n✅ [BOX OK] Valor: {valor_box_capturado:.5f}",
                         flush=True
-                        )
+                    )
+
                 else:
-                    #print(f"\r⏳ Sincronizando: {count_val}/15", end="")
 
                     print(
-                        f"\r⏳ Sincronizando: {count_val}/15", end="",
+                        f"\r⏳ Sincronizando: {count_val}/15",
+                        end="",
                         flush=True
-                        )
-                    continue # Volta pro topo para pegar o próximo pacote
+                    )
 
-            # --- ETAPA B: BUSCAR A PISTA (SÓ CHEGA AQUI SE O BOX JÁ FOI CAPTURADO) ---
-            
-            # Monitora o pico (distância percorrida)
+                    continue
+
+            # =========================================================
+            # 🏁 MONITORA PICO DA PISTA
+            # =========================================================
+
             if valor_atual > pico_atual:
                 pico_atual = valor_atual
 
-            # Se ainda não sabemos a pista, tentamos identificar pelo DNA
-            if pista_confirmada == 0:
-                for dna, nome_pista in MAPA_PISTAS.items():
-                    if abs(pico_atual - dna) < 2.0:
-                        pista_confirmada = nome_pista
-                        #print(f"\n🌍 [PISTA OK] Identificada: {pista_confirmada}")
-                        
-                        print(
-                                f"\n🌍 [PISTA OK] Identificada: {pista_confirmada}",
-                                flush=True
-                                )
-                        
-                        # ETAPA C: BINGO!
-                        carro_nome = buscar_carro(pista_confirmada, valor_box_capturado)
-                        #print(f"🏎️ [CARRO OK] Identificado: {carro_nome}")
+            # =========================================================
+            # 🌍 IDENTIFICA PISTA
+            # =========================================================
 
-                        print(
-                                f"🏎️ [CARRO OK] Identificado: {carro_nome}",
-                                flush=True
-                                )
-                        
-                        # Passa o bastão para a telemetria principal e encerra
-                        SocketF12013(sock,contador_voltas,carro_nome,pista_confirmada,ultima_volta)
-                        
+            if valor_atual < pico_atual and pista_confirmada == "":
+
+                pista = descobrir_pista(pico_atual)
+
+                if pista != "PISTA_DESCONHECIDA":
+
+                    pista_confirmada = pista
+
+                    print(
+                        f"\n🌍 [PISTA OK] Identificada: {pista_confirmada}",
+                        flush=True
+                    )
+
+                    # =========================================================
+                    # 🚗 IDENTIFICA CARRO
+                    # =========================================================
+
+                    carro_nome = buscar_carro(
+                        pista_confirmada,
+                        valor_box_capturado
+                    )
+
+                    print(
+                        f"🏎️ [CARRO OK] Identificado: {carro_nome}",
+                        flush=True
+                    )
+
+                    # =========================================================
+                    # 🚀 INICIA TELEMETRIA
+                    # =========================================================
+
+                    SocketF12013(
+                        sock,
+                        contador_voltas,
+                        carro_nome,
+                        pista_confirmada,
+                        ultima_volta
+                    )
 
     except KeyboardInterrupt:
+
         print("\n🛑 Encerrado.")
 
 def SocketF12013(sock,contador_voltas,carro_nome,pista_confirmada,ultima_volta):
-    sock.settimeout(100)
+    sock.settimeout(5)
     while True:
         try:
             data, _ = sock.recvfrom(4096)
